fa4d2bf73a83fa409e3068c04e79cb8e07d25a5a
Jenna Jiang <tqj5047@gmail.comm>
2020-10-01 13:05:36 -0400

    2

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const index = require("./views/index");
+const bodyParser = require('body-Parser')
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+// app.use(express.static('views'))
+app.use(express.static(__dirname + "/public"));
+
+app.use('/main', require('./views/main'))
 
 app.get('/', (req, res, next) => {
-    res.send("Hello World")
+    res.redirect(`/index`);
+})
+let PORT = 3000
+app.listen(PORT, () => {
+    console.log(`listening from ${PORT}`)
 })
